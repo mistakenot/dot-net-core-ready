ee1f1bd76dec7e88821ccd90ced0cb53267b1cd9
charlie <the_charlie_daly@hotmail.co.uk>
2017-08-16 00:11:51 +0100

    Trying to fix minification.

--- DotNetCoreReady/App_Start/BundleConfig.cs
+++ DotNetCoreReady/App_Start/BundleConfig.cs
@@ -9,36 +9,36 @@ namespace DotNetCoreReady
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/bootstrap3-typeahead.js",
-                      "~/Scripts/bloodhound.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js",
+                "~/Scripts/bootstrap3-typeahead.js",
+                "~/Scripts/bloodhound.js"));
 
             // http://winstrap.azurewebsites.net
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                        //"~/Content/bootstrap.css",
-                        "~/Content/winstrap.css",
-                      "~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/glyphs-winjs-symbols.ttf").Include(
+                "~/Content/winstrap.css",
+                "~/Content/site.css",
                 "~/Content/winjs-symbols.ttf"));
+
+            //bundles.Add(new StyleBundle("~/Content/glyphs-winjs-symbols.ttf").Include(
+            //    "~/Content/winjs-symbols.ttf"));
 
             bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
-                    "~/Scripts/knockout-3.4.2.js"));
+                "~/Scripts/knockout-3.4.2.js"));
 
-            bundles.Add(new StyleBundle("~/Content/winjs-symbols.ttf").Include(
-                "~/Content/winjs-symbols.ttf"));
+            //bundles.Add(new StyleBundle("~/Content/winjs-symbols.ttf").Include(
+            //    "~/Content/winjs-symbols.ttf"));
         }
     }
 }
