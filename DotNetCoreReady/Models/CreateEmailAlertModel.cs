670eb1c4645477e471fe5f0c1d3f229039a80874
Charlie Daly <the_charlie_daly@hotmail.co.uk>
2017-09-27 21:34:28 +0100

    Working post with emails in table storage.
    Working confirmation message.
    
    Trying to fix form for mobile.

---
+++ DotNetCoreReady/Models/CreateEmailAlertModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCoreReady.Models
+{
+    public class CreateEmailAlertModel
+    {
+        [MinLength(1)]
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [MinLength(1)]
+        [Required]
+        public string PackageId { get; set; }
+
+        [Required]
+        public bool OptedInToMarketing { get; set; }
+    }
+}
---
+++ DotNetCoreReady/Services/EmailAlertsRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace DotNetCoreReady.Services
+{
+    public class EmailAlertsRepository
+    {
+        private readonly CloudTable _table;
+
+        public EmailAlertsRepository(string connectionString, string tableName)
+        {
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            _table = tableClient.GetTableReference(tableName);
+        }
+
+        public async Task<CreateResult> CreateIfNotExists(string email, string packageId, bool optedInToMarketing)
+        {
+            await _table.CreateIfNotExistsAsync();
+
+            var newEntity = new EmailAlert()
+            {
+                PartitionKey = packageId,
+                RowKey = email,
+                CreatedAt = DateTime.UtcNow,
+                OptedInToMarketing = optedInToMarketing
+            };
+
+            var op = TableOperation.InsertOrReplace(newEntity);
+            var result = await _table.ExecuteAsync(op);
+            
+            if (result.HttpStatusCode != 204)
+            {
+                return new CreateResult()
+                {
+                    WasSuccessful = false,
+                    ErrorCode = result.HttpStatusCode
+                };
+            }
+
+            return new CreateResult();
+        }
+
+        private class EmailAlert : TableEntity
+        {
+            public DateTime CreatedAt { get; set; }
+
+            public bool OptedInToMarketing { get; set; }
+        }
+
+        public class CreateResult
+        {
+            public bool WasSuccessful { get; set; } = true;
+
+            public int ErrorCode { get; set; } = -1;
+        }
+    }
+}
